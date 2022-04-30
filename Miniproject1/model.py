b0e1d5af05ee91a4bd27815865396412b2ed43c2
Yuxiao Li <73948586+yetiiil@users.noreply.github.com>
2022-05-01 00:42:09 +0200

    Add files via upload

--- Miniproject1/model.py
+++ Miniproject1/model.py
@@ -65,11 +65,17 @@ class Model():
                 pool3 = self._block2(pool2)
                 pool4 = self._block2(pool3)
                 pool5 = self._block2(pool4)
+                pool6 = self._block2(pool5)
+                pool7 = self._block2(pool6)
 
                 # Decoder
-                upsample5 = self._block3(pool5)
+                upsample7 = self._block3(pool7)
+                concat7 = torch.cat((upsample7, pool6), dim=1)
+                upsample6 = self._block4(concat7)
+                concat6 = torch.cat((upsample6, pool5), dim=1)
+                upsample5 = self._block5(concat6)
                 concat5 = torch.cat((upsample5, pool4), dim=1)
-                upsample4 = self._block4(concat5)
+                upsample4 = self._block5(concat5)
                 concat4 = torch.cat((upsample4, pool3), dim=1)
                 upsample3 = self._block5(concat4)
                 concat3 = torch.cat((upsample3, pool2), dim=1)
@@ -86,15 +92,13 @@ class Model():
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
         self.model.to(self.device)
 
-        self.optimizer = optim.SGD(self.model.parameters(), lr = 1e-3)
-        self.loss_function = nn.MSELoss()
+        self.optimizer = optim.Adam(self.model.parameters(), lr = 1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0, amsgrad=True)
+        self.criterion = nn.MSELoss()
 
     def save_model(self) -> None :
         torch.save(self.model.state_dict(), 'bestmodel.pth')
 
     def load_pretrained_model(self) -> None:
-        self.model.load_state_dict(torch.load('bestmodel.pth'))
-
         if self.use_cuda:
             self.model.load_state_dict(torch.load('bestmodel.pth'))
         else:
@@ -103,10 +107,33 @@ class Model():
     def train(self, train_input, train_target) -> None:
     #:train_input: tensor of size (N, C, H, W) containing a noisy version of the images. same images, which only differs from the input by their noise.
     #:train_target: tensor of size (N, C, H, W) containing another noisy version of the
-        nb_epochs = 400
+        train_input, train_target = train_input.to(self.device).type(torch.float), train_target.to(self.device).type(torch.float)
+
+        model = self.model
+        criterion = self.criterion
+        device = self.device
+        model = model.to(device)
+        optimizer = self.optimizer
+
+        nb_epochs = 100
         mini_batch_size = 100
 
-        train_input, train_target = train_input.to(self.device), train_target.to(self.device)
+        model.train()
+        print(model)
+        print("Starting Training Loop...")
+        for epoch in range(nb_epochs):
+            print('-' * 10)
+            running_loss = 0.0
+            for b in range(0, train_input.size(0), mini_batch_size):
+                optimizer.zero_grad()
+                denoised_source = model(train_input.narrow(0, b, mini_batch_size))
+                loss = criterion(denoised_source, train_target.narrow(0, b, mini_batch_size))
+                loss.backward()
+                optimizer.step() 
+
+                running_loss += loss.item()
+            epoch_loss = running_loss / len(train_input)
+            print('{} Loss: {:.4f}'.format('current '+ str(epoch), epoch_loss))
         
     def predict(self, test_input) -> torch.Tensor:
     #:test ̇input: tensor of size (N1, C, H, W) that has to be denoised by the trained or the loaded network.
