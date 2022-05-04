b940a6b9397244d0cb67c4641547daac866925da
Yuxiao Li <liyuxiao1999@gmail.com>
2022-05-04 14:55:37 +0200

    add new files

---
+++ Miniproject2/others/module.py
@@ -0,0 +1,11 @@
+class Module(object):
+    def forward(self, *input):
+        raise NotImplementedError
+    
+    def backward(self, *gradwrtoutput):
+        raise NotImplementedError
+
+    def param(self):
+        return []
+        
+class Conv2d(Module):
