e1a9d296b5a60338076a755562dd97e38dcb014d
Bastien Le Lan <bastien.lelan@epfl.ch>
2022-05-20 14:56:37 +0200

    container and upsampling

--- Miniproject2/upsampling.py
+++
@@ -1,7 +0,0 @@
-from torch import empty
-def upsampling(ratio,input_data):
-    #its bad I don't like it :(
-    out=empty(input_data.size()[0]*ratio,input_data.size()[1]*ratio)
-    for i in range out.size()[0]:
-        for j in range out.size()[1]:
-            out[i,j]=input_data[int(i/ratio),int(j/ratio)]
